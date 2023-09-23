53ac98a8435f67c3edeb0648c09c1a1a37824847
Palak Mathur <palakmathur@gmail.com>
2023-09-23 14:51:04 -0500

    feat(App.js): show email textbox to sign up

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App" style={{backgroundImage: logo}}>
       <header className="App-header">
         <h1> The intelligence that helps your business is on its way!!</h1>
+      
+        <form id="contact-form">
+          <div className="form-group">
+            <label class="input">Keep Me Posted  </label>
+            <input type="email" className="form-control" aria-describedby="emailHelp" />
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </div>  
+        </form>
       </header>
     </div>
+ 
   );
 }
 
